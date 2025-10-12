2d28e4447bfdffa6bf631adb36648f08b04b6b9f
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-10-12 19:03:20 +0100

    added base url approach

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL:`${import.meta.env.BaseUrl}/api/`,
   withCredentials: true,
 });
 
