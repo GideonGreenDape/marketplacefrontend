5b780d3967c4766659a1af50ee051f6b09d8b2eb
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-10-12 18:49:15 +0100

    changes to file import

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Slide.scss";
-import Slider from "infinite-react-carousel";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+  const settings = {
+    slidesToShow,
+    slidesToScroll: arrowsScroll,
+    infinite: true,
+    dots: false,
+    arrows: true,
+  };
+
   return (
     <div className="slide">
       <div className="container">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+        <Slider {...settings}>
           {children}
         </Slider>
       </div>
